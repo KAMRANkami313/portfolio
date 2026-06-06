6d4dfa5b8c280224b2228e64c8354ca91b888f5f
KAMRANkami313 <muhammadkamrankami313@gmail.com>
2026-06-06 16:36:12 +0500

    Add complete favicon set: SVG, ICO, PNG icons for all platforms + Google search

--- public/sw.js
+++ public/sw.js
@@ -3,6 +3,12 @@ const STATIC_ASSETS = [
   '/',
   '/index.html',
   '/favicon.svg',
+  '/favicon.ico',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/apple-touch-icon.png',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
   '/manifest.json',
 ];
 
